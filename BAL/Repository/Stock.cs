865578dea3ba00403c2b98c63222af3b5d8e2498
VNDPAL <39481600+VNDPAL@users.noreply.github.com>
2020-04-18 22:24:05 +0530

    Update Stock.cs

--- BAL/Repository/Stock.cs
+++ BAL/Repository/Stock.cs
@@ -137,7 +137,7 @@ namespace BLL.Repository
             SingleReturnResult<StockDto> stock = new SingleReturnResult<StockDto>();
             try
             {
-                string SqlQuery = "SELECT * FROM Stock WHERE StockId = @StockId";
+                string SqlQuery = "SELECT s.*,j.ProcessStatus FROM Stock s join jobmaster j on s.jobid=j.jobid WHERE s.StockId = @StockId";
 
                 using (var connection = new SqlConnection(_conn.strConnectionString()))
                 {
